f625f708085920f0b33664c1aa950296ea7683a7
Habiba <uhabiba005@gmail.com>
2024-09-23 20:44:36 +0200

    Add authorization to  nats through usercredentials file

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
 import "fmt"
+import "github.com/nats-io/nats.go"
 
 func main() {
-    fmt.Println("Hello World!")
+    fmt.Println("Connecting to nats")
+	url := "nats://nats-dev.dpp.vwapps.run:4222"
+	nc, err := nats.Connect(url, nats.UserCredentials("/home/habiba/projects/nats-authorization/default.creds"))
+
+	if err != nil {
+		fmt.Printf("Error connecting to Nats; %s\n", err.Error())
+		return
+	}
+
+	fmt.Printf("Connected to Nats successfully; %s\n", nc)
 }
